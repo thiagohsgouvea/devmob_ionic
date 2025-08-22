5a81a93edab1cf715c3a37bb58a852beed53da70
Thiago Gouvêa <thiagogouvea@gmail.com>
2025-08-22 00:48:00 +0000

    criacao pasta app1

---
+++ app1/app/vite/deps/ios.transition-R7QDFWL4.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-GHDEDF4D.js";
+import "./chunk-TN6KSJHA.js";
+import "./chunk-GGFSAU2Q.js";
+import "./chunk-QEE7QVES.js";
+import "./chunk-4554YRK6.js";
+import "./chunk-2H3NLAAY.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
---
+++ app1/app/vite/deps/md.transition-C6O2BE67.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-Y5NC4DKV.js";
+import "./chunk-TN6KSJHA.js";
+import "./chunk-GGFSAU2Q.js";
+import "./chunk-QEE7QVES.js";
+import "./chunk-4554YRK6.js";
+import "./chunk-2H3NLAAY.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  mdTransitionAnimation
+};
